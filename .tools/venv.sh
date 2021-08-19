a13a42c2a4a3711656ae73ca35aac26fab50f022
nhac43 <nakata.h.ac@gmail.com>
2021-08-19 21:18:43 +0900

    update

--- .tools/venv.sh
+++ .tools/venv.sh
@@ -5,12 +5,6 @@ function vc() {
         pyenv local $1
     fi
     python -m venv .venv
-} 
-function vca() {
-    if [ $# != 0 ]; then
-        pyenv local $1
-    fi
-    python -m venv .venv
     source .venv/bin/activate
 } 
 alias va='source .venv/bin/activate'
