52147a7cb27ed033f6e190eef00c9a2cf939e27a
nhac43 <nakata.h.ac@gmail.com>
2021-09-16 01:37:06 +0900

    update ivim

--- .tools/tmux.sh
+++ .tools/tmux.sh
@@ -4,7 +4,6 @@ function ivim(){
     tmux split-window -v
     tmux resize-pane -D 15
     tmux split-window -h
-    tmux resize-pane -R 75
     tmux select-pane -t 0
     tmux send -t 2 'tig status' Enter
     vim
