73583614cd0a6dd40abcb0eca97c3bc15a5b8a14
nhac43 <nakata.h.ac@gmail.com>
2024-06-15 15:48:33 +0900

    replace CD_HISTORY_PATH

--- tools/cd.sh
+++ tools/cd.sh
@@ -1,7 +1,8 @@
 export CD_HISTORY_LIMIT=100
+export CD_HISTORY_PATH=$HOME/.cd_history
 cd() {
     if [ -z "$1" ] || [ "$1" = "-" ]; then
-        target_path=$(tail -r $HOME/.cd_history | sed '1d' | fzf)
+        target_path=$(tail -r $CD_HISTORY_PATH | sed '1d' | fzf)
         if [ ! -e "$target_path" ]; then
             return
         fi
@@ -12,16 +13,16 @@ cd() {
 
     if [ -e "$target_path" ]; then
         # .cd_history が存在し、かつ最後の行が target_path でない場合に追記
-        if [ ! -f "$HOME/.cd_history" ]; then
-            echo "$target_path" >> "$HOME/.cd_history"
-        elif [ "$(tail -n 1 "$HOME/.cd_history")" != "$target_path" ]; then
-            echo "$target_path" >> "$HOME/.cd_history"
+        if [ ! -f "$CD_HISTORY_PATH" ]; then
+            echo "$target_path" >> "$CD_HISTORY_PATH"
+        elif [ "$(tail -n 1 "$CD_HISTORY_PATH")" != "$target_path" ]; then
+            echo "$target_path" >> "$CD_HISTORY_PATH"
         fi
         if [ -n "$CD_HISTORY_LIMIT" ]; then
-            current_lines=$(wc -l < "$HOME/.cd_history")
+            current_lines=$(wc -l < "$CD_HISTORY_PATH")
             while [ "$current_lines" -gt "$CD_HISTORY_LIMIT" ]; do
-                sed -i '1d' "$HOME/.cd_history"
-                current_lines=$(wc -l < "$HOME/.cd_history")
+                sed -i '1d' "$CD_HISTORY_PATH"
+                current_lines=$(wc -l < "$CD_HISTORY_PATH")
             done
         fi
         builtin cd "$target_path"
