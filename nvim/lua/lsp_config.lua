d4a311e931e82bd39759221dbd64d57867027b3a
nhac43 <nakata.h.ac@gmail.com>
2024-06-29 00:09:25 +0900

    update lsp settings

--- nvim/lua/lsp_config.lua
+++ nvim/lua/lsp_config.lua
@@ -1,31 +1,59 @@
 require("mason").setup()
-require("mason-lspconfig").setup({
-	ensure_installed = {
-		"pyright",
-		-- "lua_ls",
-	},
-})
+-- require("mason-lspconfig").setup({
+-- 	ensure_installed = {
+-- 		"ruff_lsp",
+-- 		"pyright",
+-- 		-- "lua_ls",
+-- 	},
+-- })
 
-require("lspconfig").pyright.setup({})
-require("lspconfig").lua_ls.setup({})
-require("lspconfig").rust_analyzer.setup({
+-- Configure `ruff-lsp`.
+-- See: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#ruff_lsp
+-- For the default config, along with instructions on how to customize the settings
+local on_attach = function(client, bufnr)
+  if client.name == 'ruff_lsp' then
+    -- Disable hover in favor of Pyright
+    client.server_capabilities.hoverProvider = false
+  end
+end
+
+require('lspconfig').ruff_lsp.setup {
+  on_attach = on_attach,
+}
+require('lspconfig').pyright.setup {
   settings = {
-    ["rust-analyzer"] = {
-      check = {
-        command = "clippy",
+    pyright = {
+      -- Using Ruff's import organizer
+      disableOrganizeImports = true,
+    },
+    python = {
+      analysis = {
+        -- Ignore all files for analysis to exclusively use Ruff for linting
+        ignore = { '*' },
       },
     },
   },
-})
+}
+
+-- Not used
+-- require("lspconfig").lua_ls.setup({})
+-- require("lspconfig").rust_analyzer.setup({
+--   settings = {
+--     ["rust-analyzer"] = {
+--       check = {
+--         command = "clippy",
+--       },
+--     },
+--   },
+-- })
 
 local null_ls = require("null-ls")
 null_ls.setup({
 	sources = {
-		null_ls.builtins.formatting.black, -- python formatter
-		null_ls.builtins.formatting.isort, -- python import sort
-		null_ls.builtins.diagnostics.flake8, -- python linter
+		null_ls.builtins.formatting.ruff, -- python formatter
+		null_ls.builtins.diagnostics.ruff, -- python linter
 		-- null_ls.builtins.formatting.stylua, -- lua formatter
 		-- null_ls.builtins.diagnostics.luacheck, -- lua linter
-        null_ls.builtins.formatting.rustfmt, -- Rust formatter
+        -- null_ls.builtins.formatting.rustfmt, -- Rust formatter
 	},
 })
