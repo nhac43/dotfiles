fbc7156c7539c012e06b19d49a82861f3d0612c0
nhac43 <nakata.h.ac@gmail.com>
2025-08-23 11:33:55 +0900

    move files to legacy

--- tools/loader.sh
+++ tools/loader.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 
 source $DOTTOOLS/pyenv.sh
-source $DOTTOOLS/fzf.sh
 source $DOTTOOLS/venv.sh
-source $DOTTOOLS/tmux.sh
-source $DOTTOOLS/jupyter.sh
-source $DOTTOOLS/clipboard.sh
 source $DOTTOOLS/cd.sh
-source $DOTTOOLS/git.sh
-source $DOTTOOLS/ssh.sh
-# source $DOTTOOLS/mise.sh
