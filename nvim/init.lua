3844d648eec37b9e8bbf42f399fde3cf6c1521c2
nhac43 <nakata.h.ac@gmail.com>
2026-02-07 03:01:49 +0900

    add fold commands for vscode

--- nvim/init.lua
+++ nvim/init.lua
@@ -72,6 +72,29 @@ if is_vscode then
   -- help_tags 相当（コマンドパレット）
   vim.keymap.set("n", "<Leader>fh",
     "<Cmd>call VSCodeNotify('workbench.action.showCommands')<CR>")
+  
+  -- ===== Fold (z系) =====
+
+  -- toggle fold
+  vim.keymap.set("n", "za",
+    "<Cmd>call VSCodeNotify('editor.toggleFold')<CR>")
+
+  -- close fold
+  vim.keymap.set("n", "zc",
+    "<Cmd>call VSCodeNotify('editor.fold')<CR>")
+
+  -- open fold
+  vim.keymap.set("n", "zo",
+    "<Cmd>call VSCodeNotify('editor.unfold')<CR>")
+
+  -- fold all
+  vim.keymap.set("n", "zM",
+    "<Cmd>call VSCodeNotify('editor.foldAll')<CR>")
+
+  -- unfold all
+  vim.keymap.set("n", "zR",
+    "<Cmd>call VSCodeNotify('editor.unfoldAll')<CR>")
+    
 end
 
 -- =========================
